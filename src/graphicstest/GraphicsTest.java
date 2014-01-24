79026ac1903fd38c945098645e17b91b4d116a47
Connor O'Hara <chuckhead7@gmail.com>
2014-01-24 18:09:25 -0500

    Now it uses Slick
    LWJGL wasn't meant to make games with. Maybe one day I'll learn opengl.

--- src/graphicstest/GraphicsTest.java
+++ src/graphicstest/GraphicsTest.java
@@ -1,49 +1,43 @@
 package graphicstest;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.util.glu.GLU.*;
-import java.io.IOException;
-import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.lwjgl.LWJGLException;
-import org.lwjgl.input.Keyboard;
-import org.lwjgl.input.Mouse;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.GL11;
-/**
- * @author jediTofu
- * @see <a href="http://lwjgl.org/">LWJGL Home Page</a>
- */
-public class GraphicsTest {
-    public void start() {
-        try {
-            Display.setDisplayMode(new DisplayMode(800,600));
-            Display.create();
-        }
-        catch (LWJGLException e) {
-            e.printStackTrace();
-            System.exit(0);
-        }
-        GL11.glMatrixMode(GL11.GL_PROJECTION);
-        GL11.glLoadIdentity();
-        GL11.glOrtho(0, 800, 0, 600, 1, -1);
-        GL11.glMatrixMode(GL11.GL_MODELVIEW);
-        while (!Display.isCloseRequested()) {
-            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-            GL11.glColor3f(0.5f, 0.5f, 1.0f);
-	    GL11.glBegin(GL11.GL_QUADS);
-	        GL11.glVertex2f(100,100);
-		GL11.glVertex2f(100+200,100);
-		GL11.glVertex2f(100+200,100+200);
-		GL11.glVertex2f(100,100+200);
-	    GL11.glEnd();
-            Display.update();
-        }
-        Display.destroy();
-    }
-    public static void main(String[] args) {
-        GraphicsTest graphicstest = new GraphicsTest();
-        graphicstest.start();
-    }
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class GraphicsTest extends BasicGame
+{
+	public GraphicsTest(String gamename)
+	{
+		super(gamename);
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException
+	{
+		g.drawString("Howdy!", 10, 10);
+	}
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new GraphicsTest("Simple Slick Game"));
+			appgc.setDisplayMode(640, 480, false);
+			appgc.start();
+		}
+		catch (SlickException ex)
+		{
+			Logger.getLogger(GraphicsTest.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 }
